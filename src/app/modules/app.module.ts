99356211b190cea6df248ec1829096b05d77e58e
Dmitriy Shnitkovskiy <dm.shnit@gmail.com>
2018-05-13 10:01:43 +0300

    add dialog component, init angular material

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -3,35 +3,48 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import {MatDialogModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material';
+import {MatInputModule} from '@angular/material';
 
 import {HttpService} from '../services/http.service';
+import {LocalStorageService} from '../services/localstorage.service';
 
 import {AppComponent} from '../components/app.component';
 import {ConversationComponent} from '../components/conversation-component/conversation.component'
-import { LocalStorageService } from '../services/localstorage.service';
+import {Dialog} from '../components/dialog-component/dialog.component';
 
 const appRoutes: Routes = [
     {
         path: 'conversation',
         component: ConversationComponent
     },
-    {
-        path: '**', 
-        redirectTo: 'conversation'
-    }
+    // {
+    //     path: '**', 
+    //     redirectTo: 'conversation'
+    // }
 ];
 
 @NgModule({
   declarations: [
       AppComponent,
-      ConversationComponent
+      ConversationComponent,
+      Dialog,
   ],
+  entryComponents: [
+    Dialog
+    ],
   imports: [
       BrowserModule,
       HttpClientModule,
       RouterModule.forRoot(appRoutes),
-      FormsModule
-  ],
+      FormsModule,
+      MatDialogModule,
+      BrowserAnimationsModule,
+      MatFormFieldModule,
+      MatInputModule
+    ],
   providers: [HttpService, LocalStorageService],
   bootstrap: [AppComponent]
 })
